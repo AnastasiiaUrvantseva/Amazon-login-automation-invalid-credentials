ec7370a2caa4d67f618b5f31fa51a7f7fbd23818
AnastasiiaUrvantseva <ana.urvantseva@gmail.com>
2025-01-30 17:25:48 -0300

    Initial commit with Amazon login automation test for invalid credentials

---
+++ Selenium_Automation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium_Automation
+{
+    internal class Program
+    {
+        // Create a reference for Chrome browser
+        IWebDriver driver = new ChromeDriver();
+
+        
+        static void Main(string[] args)
+        {
+                      
+        }
+        [SetUp]
+        public void Initialize()
+        {
+            // Go to Google Page
+            driver.Navigate().GoToUrl("https://www.amazon.com");
+        }
+
+        [Test]
+        public void ExecuteTheTest()
+        {
+            // Make the browser full screen
+            driver.Manage().Window.Maximize();
+
+            IWebElement SignIn = driver.FindElement(By.Id("nav-link-accountList"));
+            SignIn.Click();
+
+            IWebElement EmailField = driver.FindElement(By.Id("ap_email"));
+            EmailField.SendKeys("pewpew@abv.bg");
+
+            IWebElement ContinueButton = driver.FindElement(By.ClassName("a-button-input"));
+            ContinueButton.Click();
+
+            IWebElement ErrorMessage = driver.FindElement(By.ClassName("a-alert-heading"));
+            string ActualErrorMessageText = ErrorMessage.Text;
+            string ExpectedErrorMessageText = "There was a problem";
+            //Assert.That(Equals(ActualErrorMessageText, ExpectedErrorMessageText));
+            NUnit.Framework.Legacy.ClassicAssert.AreEqual(ExpectedErrorMessageText, ActualErrorMessageText);
+
+        }
+        [TearDown]
+        public void CloseTest()
+        {
+            // Close the browser
+            driver.Quit();
+        }
+    }
+}
